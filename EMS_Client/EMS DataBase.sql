3b446517241bd42c773bb54a6ac5d8a56e857ca6
Alex Kozak <a.kozak1@outlook.com>
2019-04-04 19:04:24 -0400

    Update EMS DataBase.sql
    Changed a number of the sizes of strings, and some names to fit the standard.

--- EMS_Client/EMS DataBase.sql
+++ EMS_Client/EMS DataBase.sql
@@ -4,11 +4,7 @@
 *  \author The Char Stars - alex , attila, div, tudor
 *  \date 2019-04-04
 *  \brief Patient class definition and functions
-*  
-*  
 */
-
-
 
 
 /*creating the database called EMS2 in to SQL server*/
@@ -20,18 +16,18 @@ USE EMS2;		/*here we tell SSMS that from here we everything we create or delete,
 /* creating the patient table which will hold the patients profile details*/
 Create table tblPatients (
 	PatientID int IDENTITY(0,1) PRIMARY KEY,
-	FirstName varchar(50),
-	LastName varchar(50),
+	FirstName varchar(64),
+	LastName varchar(64),
 	HCN varchar(12),
 	MInitial varchar(1),
 	DateOfBirth date,
-	Sex varchar(1),
+	Gender varchar(1),
 	HeadOfHouse varchar(12),
-	AddressLine1 varchar(50),
-	AddressLine2 varchar(50),
-	City varchar(10),
+	AddressLine1 varchar(64),
+	AddressLine2 varchar(64),
+	City varchar(64),
 	Province varchar(2),
-	Postal_Code varchar(6),
+	PostalCode varchar(10),
 	PhoneNum int
 );
 
@@ -40,36 +36,36 @@ create table tblAppointments(
 	AppointmentID int IDENTITY(0,1) PRIMARY KEY,
 	PatientID int NOT NULL FOREIGN KEY REFERENCES tblPatients(PatientID),
 	DependantID int FOREIGN KEY REFERENCES tblPatients(PatientID),
-	Appointment_Notes varchar(50)
+	AppointmentNotes varchar(512)
 );
 
-/* creating the schedual table where EMS will store it all the Appointment wich has been */
+/* creating the schedule table where EMS will store it all the Appointment which has been */
 create table tblSchedules(
 	AppointmentID int NOT NULL FOREIGN KEY REFERENCES tblAppointments(AppointmentID),
-	Appointment_Date date NOT NULL,
-	Appointment_TimeSlot int NOT NULL
+	AppointmentDate date NOT NULL,
+	AppointmentTimeSlot int NOT NULL
 );
 
 
 /* creating billing codes table which will contain all the billing records which EMS has generated*/ 
 create table tblBillingCodes(
-	Billing_code varchar(25) NOT NULL PRIMARY KEY,
+	BillingCode varchar(25) NOT NULL PRIMARY KEY,
 	Effective_Date date,
-	cost decimal
+	Cost decimal
 );
 
 
-/*creating the appointment billing record which will contain infromation about which billing records is from which appointment*/
+/*creating the appointment billing record which will contain information about which billing records is from which appointment*/
 create table tblAppointmentBillingRecords(
 	AppointmentID int NOT NULL FOREIGN KEY REFERENCES tblAppointments(AppointmentID),
 	PatientID int NOT NULL FOREIGN KEY REFERENCES tblPatients(PatientID),
 	BillingCode varchar(25) NOT NULL FOREIGN KEY REFERENCES tblBillingCodes(Billing_code),
 );
 
-/*creating user information which will contains the user login infromation*/
+/*creating user information which will contains the user login information*/
 create table tblUsers(
 	UserID int IDENTITY(0,1) PRIMARY KEY,
-	UserName varchar(25),
-	User_Password varchar(256),
-	accessLevel int	
+	UserName varchar(256),
+	Password varchar(256),
+	AccessLevel int	
 );
